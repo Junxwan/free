434024b07acf2589db4ea248b3985044da474062
Junx <hugh8845sam@gmail.com>
2023-04-09 18:07:41 +0800

    feat: 判斷文件是否存在

--- data/op.go
+++ data/op.go
@@ -3,6 +3,7 @@ package data
 import (
 	"bytes"
 	"fmt"
+	file "github.com/Junxwan/free/lib"
 	"github.com/go-gota/gota/dataframe"
 	"golang.org/x/text/encoding/unicode"
 	"golang.org/x/text/transform"
@@ -12,20 +13,30 @@ import (
 )
 
 func DownLoadOPChips(time time.Time, path string) error {
-	chips, err := downLoadOPChips(time)
-	if err != nil {
-		return err
-	}
-
-	// 因為期交所csv每個row結尾都多一個,會跟column數量對不上出現wrong number of fields
-	// 解決方式就是在多一個空column
-	chips = bytes.Replace(chips, []byte("漲跌%"), []byte("漲跌%,"), 1)
-	df := dataframe.ReadCSV(bytes.NewReader(chips))
-
 	path = fmt.Sprintf("%s/%s.csv",
 		filepath.Join(path, "op", "day"),
 		time.Format("2006-01-02"),
 	)
+
+	if !file.IsExist(path) {
+		body, err := downLoadOPChips(time)
+		if err != nil {
+			return fmt.Errorf("downLoadOPChips error: %w", err)
+		}
+
+		if err := saveOPRawChips(body, path); err != nil {
+			return fmt.Errorf("saveOPRawChips error: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func saveOPRawChips(body []byte, path string) error {
+	// 因為期交所csv每個row結尾都多一個,會跟column數量對不上出現wrong number of fields
+	// 解決方式就是在多一個空column
+	body = bytes.Replace(body, []byte("漲跌%"), []byte("漲跌%,"), 1)
+	df := dataframe.ReadCSV(bytes.NewReader(body))
 
 	f, err := os.Create(path)
 	if err != nil {
