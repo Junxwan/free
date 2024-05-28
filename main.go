9de6e6e3a2c04821ff161a6ec5d256426fcf1a4b
Junx <hugh8845sam@gmail.com>
2024-05-29 04:01:54 +0800

    .

--- main.go
+++ main.go
@@ -8,11 +8,14 @@ import (
 	"os"
 	"sort"
 	"strconv"
+	"strings"
 	"time"
 )
 
 type K struct {
 	Time time.Time
+	S1   string
+	S2   string
 	D    int64
 	O    int64
 	H    int64
@@ -21,12 +24,85 @@ type K struct {
 	V    int64
 }
 
-func main() {
-	var data []K
+var data map[string][]K
 
+func main() {
+	data = make(map[string][]K)
+
+	load("5")
+	load("30")
+	load("60")
+	loadDay()
+	loadWeek()
+	loadMonth()
+
+	router := gin.New()
+
+	router.Static("/public", "./public")
+
+	var Time struct {
+		T     string `form:"t"`
+		Start int64  `form:"start"`
+		End   int64  `form:"end"`
+	}
+
+	router.GET("/kline", func(c *gin.Context) {
+		c.ShouldBindQuery(&Time)
+
+		if Time.T == "" {
+			Time.T = "5"
+		}
+
+		vv := data[Time.T]
+
+		var min, max int
+
+		d := [][]int64{}
+		if Time.Start > 0 {
+			for i, v := range vv {
+				if min == 0 && Time.Start <= v.D {
+					min = i
+				}
+
+				if min != 0 && Time.End <= v.D {
+					max = i
+					break
+				}
+			}
+		} else {
+			min = 0
+			max = len(vv)
+			switch Time.T {
+			case "5":
+				min = max - 1200
+			case "30":
+				min = max - 1200
+			case "60":
+				min = max - 1200
+			}
+		}
+
+		for _, v := range vv[min:max] {
+			d = append(d, []int64{
+				v.D, v.O, v.H, v.L, v.C, v.V,
+			})
+		}
+
+		c.JSON(http.StatusOK, d)
+	})
+
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	router.Run()
+}
+
+func load(t string) {
+	d := make([]K, 0, 10000)
 	y := time.Now().Year()
-	for i := 0; i < 2; i++ {
-		file, err := os.Open(fmt.Sprintf("./data/tfe-tx00-%d-5min.csv", y-i))
+	for i := 0; i < 10; i++ {
+		file, err := os.Open(fmt.Sprintf("./data/tfe-tx00-%d-%smin.csv", y-i, t))
 		if err != nil {
 			panic(err)
 		}
@@ -49,67 +125,165 @@ func main() {
 			h, _ := strconv.ParseInt(v[4], 10, 64)
 			l, _ := strconv.ParseInt(v[5], 10, 64)
 			c, _ := strconv.ParseInt(v[6], 10, 64)
-			v, _ := strconv.ParseInt(v[7], 10, 64)
+			vv, _ := strconv.ParseInt(v[7], 10, 64)
 
-			data = append(data, K{
+			s := strings.Split(v[2], " ")
+
+			d = append(d, K{
 				Time: t,
+				S1:   s[0],
+				S2:   s[1],
 				D:    t.UnixMilli(),
 				O:    o,
 				H:    h,
 				L:    l,
 				C:    c,
-				V:    v,
+				V:    vv,
 			})
 		}
 	}
 
-	sort.Slice(data, func(i, j int) bool {
-		return data[i].D < data[j].D
+	sort.Slice(d, func(i, j int) bool {
+		return d[i].D < d[j].D
 	})
 
-	router := gin.New()
+	data[t] = d
+}
 
-	router.Static("/public", "./public")
+func loadDay() {
+	var start int
+	var open K
 
-	var Time struct {
-		Start int64 `form:"start"`
-		End   int64 `form:"end"`
-	}
+	vv := data["60"]
 
-	router.GET("/kline", func(c *gin.Context) {
-		c.ShouldBindQuery(&Time)
-
-		var min, max int
-
-		d := [][]int64{}
-		if Time.Start > 0 {
-			for i, v := range data {
-				if min == 0 && Time.Start <= v.D {
-					min = i
-				}
-
-				if min != 0 && Time.End <= v.D {
-					max = i
-					break
-				}
+	for i, v := range vv {
+		// 開始有夜盤
+		if v.S1 >= "2017-5-15" {
+			if v.S2 == "16:00:00" {
+				open = v
+				start = i
 			}
 		} else {
-			min = len(data) - 360
-			max = len(data)
+			if v.S2 == "09:45:00" {
+				open = v
+				start = i
+			}
 		}
 
-		for _, v := range data[min:max] {
-			d = append(d, []int64{
-				v.D, v.O, v.H, v.L, v.C, v.V,
-			})
+		if v.S2 == "13:45:00" {
+			t, _ := time.Parse("2006-01-02", v.S1)
+
+			k := K{
+				Time: t,
+				S1:   v.S1,
+				S2:   v.S1,
+				D:    t.UnixMilli(),
+				O:    open.O,
+				C:    v.C,
+				H:    v.H,
+				L:    v.L,
+			}
+
+			d := vv[start : i+1]
+			for _, v := range d {
+				k.V = k.V + v.V
+
+				if v.H > k.H {
+					k.H = v.H
+				}
+
+				if v.L < k.L {
+					k.L = v.L
+				}
+			}
+
+			data["day"] = append(data["day"], k)
+		}
+	}
+}
+
+func loadWeek() {
+	var start int
+	var open K
+
+	vv := data["day"]
+
+	for i, v := range vv {
+		if v.Time.Weekday() == time.Monday {
+			open = v
+			start = i
 		}
 
-		c.JSON(http.StatusOK, d)
-	})
+		if v.Time.Weekday() == time.Friday {
+			t, _ := time.Parse("2006-01-02", v.S1)
 
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{})
-	})
+			k := K{
+				Time: t,
+				S1:   open.S1,
+				S2:   v.S1,
+				D:    t.UnixMilli(),
+				O:    open.O,
+				C:    v.C,
+				H:    v.H,
+				L:    v.L,
+			}
 
-	router.Run()
+			d := vv[start : i+1]
+			for _, v := range d {
+				//k.V = k.V + v.V
+
+				if v.H > k.H {
+					k.H = v.H
+				}
+
+				if v.L < k.L {
+					k.L = v.L
+				}
+			}
+
+			data["week"] = append(data["week"], k)
+		}
+	}
+}
+
+func loadMonth() {
+	var start int
+	last := data["day"][0].S1[:7]
+
+	vv := data["day"]
+	for i, v := range vv {
+		if v.S1[:7] != last {
+			last = v.S1[:7]
+
+			t, _ := time.Parse("2006-01-02", v.S1)
+
+			k := K{
+				Time: t,
+				S1:   vv[start].S1,
+				S2:   vv[i-1].S1,
+				D:    t.UnixMilli(),
+				O:    vv[start].O,
+				C:    vv[i-1].C,
+				H:    v.H,
+				L:    v.L,
+			}
+
+			d := vv[start:i]
+			for _, v := range d {
+				//k.V = k.V + v.V
+
+				if v.H > k.H {
+					k.H = v.H
+				}
+
+				if v.L < k.L {
+					k.L = v.L
+				}
+			}
+
+			data["month"] = append(data["month"], k)
+
+			start = i
+		}
+	}
 }
---
+++ public/app.js
@@ -0,0 +1,331 @@
+function k(t) {
+     // 取得當前日期
+
+
+    const dataURL = 'http://127.0.0.1:8080/kline';
+        (async () => {
+            // Load the dataset
+            const data = await fetch(
+                'http://127.0.0.1:8080/kline?t=' + t
+            ).then(response => response.json());
+
+            // split the data set into ohlc and volume
+            const ohlc = [],
+                volume = [],
+                dataLength = data.length;
+
+            let previousCandleClose = 0;
+            for (let i = 0; i < dataLength; i++) {
+                ohlc.push([
+                    data[i][0], // the date
+                    data[i][1], // open
+                    data[i][2], // high
+                    data[i][3], // low
+                    data[i][4] // close
+                ]);
+
+                volume.push({
+                    x: data[i][0], // the date
+                    y: data[i][5], // the volume
+                    color: data[i][4] > previousCandleClose ? '#466742' : '#a23f43',
+                    labelColor: data[i][4] > previousCandleClose ? '#51a958' : '#ea3d3d'
+                });
+                previousCandleClose = data[i][4];
+            }
+
+            Highcharts.setOptions({
+                chart: {
+                    backgroundColor: '#0a0a0a'
+                },
+                title: {
+                    style: {
+                        color: '#cccccc'
+                    }
+                },
+                xAxis: {
+                    gridLineColor: '#181816',
+                    labels: {
+                        style: {
+                            color: '#9d9da2'
+                        }
+                    }
+                },
+                yAxis: {
+                    gridLineColor: '#181816',
+                    labels: {
+                        style: {
+                            color: '#9d9da2'
+                        }
+                    }
+                },
+                tooltip: {
+                    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                    style: {
+                        color: '#cdcdc9'
+                    }
+                },
+                scrollbar: {
+                    barBackgroundColor: '#464646',
+                    barBorderRadius: 0,
+                    barBorderWidth: 0,
+                    buttonBorderWidth: 0,
+                    buttonArrowColor: '#cccccc',
+                    rifleColor: '#cccccc',
+                    trackBackgroundColor: '#121211',
+                    trackBorderRadius: 0,
+                    trackBorderWidth: 1,
+                    trackBorderColor: '#464646'
+                },
+                exporting: {
+                    enabled: false
+                },
+                lang: {
+                    shortMonths: ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月'],
+                    weekdays: ['星期日', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六']
+                }
+            });
+
+           const chart =  Highcharts.stockChart('container_' + t, {
+                rangeSelector: {
+                    enabled: true,
+                    buttons: [{
+                        type: 'day',
+                        count: 1,
+                        text: '1d'
+                    }, {
+                        type: 'week',
+                        count: 1,
+                        text: '1w'
+                    }, {
+                        type: 'month',
+                        count: 1,
+                        text: '1m'
+                    },{
+                        type: 'month',
+                         count: 6,
+                        text: '6m'
+                    },{
+                      type: 'year',
+                       count: 1,
+                      text: '1y'
+                  }, {type: 'year',
+                       count: 2,
+                      text: '2y'
+                  },{type: 'year',
+                       count: 3,
+                      text: '3y'
+                  },{
+                        type: 'all',
+                        text: 'All'
+                    }],
+
+                    inputDateFormat: '%Y-%m-%d',
+                    inputEditDateFormat: '%Y-%m-%d'
+                },
+
+                navigator: {
+                   enabled: true,
+                   adaptToUpdatedData: true
+                },
+                title: {
+                    text: '台指期-' + t
+                },
+
+                plotOptions: {
+                    series: {
+                        point: {
+                            events: {
+                                mouseOver: function () {
+                                    this.series.chart.xAxis[0].drawCrosshair({plotX: this.plotX});
+                                    this.series.chart.yAxis[0].drawCrosshair({plotY: this.plotY});
+                                }
+                            }
+                        },
+                        cursor: 'crosshair',
+                        dataGrouping: {
+                            enabled: false
+                        },
+
+                    },
+                    candlestick: {
+                        color: 'white',
+                        upColor: 'red',
+                        upLineColor: 'red',
+                        lineColor: 'white'
+                    }
+                },
+
+                scrollbar: {
+                    enabled: true
+                },
+
+                xAxis: {
+                    minRange: 1,
+                    min: Date.UTC(2015, 0, 1),
+                    max: Date.now(),
+                    gridLineWidth: 1,
+                    crosshair: {
+                        snap: false
+                    }
+                },
+
+                yAxis: [{
+                    height: '70%',
+                    crosshair: {
+                        snap: false
+                    },
+                    accessibility: {
+                        description: 'price'
+                    }
+                }, {
+                    top: '70%',
+                    height: '30%',
+                    accessibility: {
+                        description: 'volume'
+                    }
+                }],
+
+                tooltip: {
+                    shared: true,
+                    split: false,
+                    useHTML: true,
+                    shadow: false,
+                    positioner: function () {
+                        return {x: 50, y: 10};
+                    }
+                },
+
+                series: [{
+                    type: 'candlestick',
+                    id: 'price',
+                    name: 'AAPL Stock Price',
+                    data: ohlc,
+                    tooltip: {
+                        valueDecimals: 2,
+                        pointFormat: '<b>O</b> <span style="color: {point.color}">' +
+                            '{point.open} </span>' +
+                            '<b>H</b> <span style="color: {point.color}">' +
+                            '{point.high}</span><br/>' +
+                            '<b>L</b> <span style="color: {point.color}">{point.low} ' +
+                            '</span>' +
+                            '<b>C</b> <span style="color: {point.color}">' +
+                            '{point.close}</span><br/>'
+                    }
+                }, {
+                    type: 'sma',
+                    id: '5Ma',
+                    name: '5Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#ff8c00',
+                    params: {
+                        period: 5
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }, {
+                    type: 'sma',
+                    id: '10Ma',
+                    name: '10Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#00ffff',
+                    params: {
+                        period: 10
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }, {
+                    type: 'sma',
+                    id: '20Ma',
+                    name: '20Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#0a932f',
+                    params: {
+                        period: 20
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }, {
+                    type: 'sma',
+                    id: '60Ma',
+                    name: '60Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#d4b40f',
+                    params: {
+                        period: 60
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }, {
+                    type: 'sma',
+                    id: '120Ma',
+                    name: '120Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#d40f33',
+                    params: {
+                        period: 120
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }, {
+                    type: 'sma',
+                    id: '240Ma',
+                    name: '240Ma',
+                    linkedTo: 'price',
+                    zIndex: 1,
+                    lineWidth: 0.5,
+                    color: '#720fd4',
+                    params: {
+                        period: 240
+                    },
+                    marker: {
+                        enabled: false,
+                        states: {
+                            hover: {
+                                enabled: false,
+                            }
+                        }
+                    }
+                }]
+            });
+        })();
+    }
